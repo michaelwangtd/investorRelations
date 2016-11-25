53788e94f2c2c0f265f39c10fc2a20879529b622
CaptWang <michaelwangtd@gmail.com>
2016-11-25 17:45:31 +0800

    init code

---
+++ index.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+
+import os
+"""
+作为整个框架的入口文件
+"""
+
+# 框架根目录
+rootPath = os.path.dirname(__file__)
---
+++ utils/io.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+
+import index
+
+def loadData2Json():
+    '''
+
+    '''
+
